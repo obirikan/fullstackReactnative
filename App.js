c7032f25af29bd9047548f3e4cad96a31f5847b9
obirikan <obirikan020@gmail.com>
2022-10-03 16:25:35 +0000

    ++components

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.txt}>Hello world</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +16,9 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+   
   },
+  txt:{
+    fontSize:60
+  }
 });
